2f2ecde1eab852eeb24b890a794465ea8440acf6
Veerendra <veerendrav2@gmail.com>
2017-10-16 13:02:41 +0530

    Update run.sh

--- complete/run.sh
+++ complete/run.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 java -Dserver.port=8095 -jar /gs-rest-service-0.1.0.jar &
+hostname="hostname: `hostname`"
+sed -i "s/# hostname: mymachine.mydomain/$hostname/g" /etc/dd-agent/datadog.conf
 service datadog-agent start
 while true; do
   sleep 1000
